c5a164c27ecd7f39ecc559ad02ff22b5bc26a229
HemasaiAmujuri <117273575+HemasaiAmujuri@users.noreply.github.com>
2024-03-07 12:16:16 +0530

    Add files via upload

---
+++ Mock Test API.py
@@ -0,0 +1,42 @@
+from pymongo import MongoClient
+from fastapi import FastAPI
+
+app = FastAPI()
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['MockTestDatabase']
+
+# Define endpoint to fetch questions based on branch and number of questions
+@app.get('/questions')
+def get_questions(body: dict):
+    branch = str(body['branch'])
+    num_questions = int(body['num_questions'])
+
+    # Define the percentages for each category
+    percentages = {
+        branch: 50,
+        "APTITUDE": 18,
+        "REASONING": 16,
+        "VERBAL": 16
+    }
+
+    # Calculate the number of questions for each category based on the percentages
+    num_questions_per_category = {category: int(num_questions * (percentage / 100)) for category, percentage in percentages.items()}
+
+    # Fetch questions from MongoDB based on the calculated number of questions for each category
+    formatted_questions = {}
+    for category, num_questions in num_questions_per_category.items():
+        questions = list(db[category].aggregate([{'$sample': {'size': num_questions}}]))
+        formatted_questions[category] = [{'id': str(q['_id']), 'question': q['question'], 'options': q['options']} for q in questions]
+        
+    return {'questions': formatted_questions,}
+
+
+@app.get('/')
+def home():
+    return {"message": "hello world"}
+
+# Run the FastAPI app
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost")
